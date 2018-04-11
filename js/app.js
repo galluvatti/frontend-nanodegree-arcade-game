0cddec79bae45ae6b9df99864496de2bc485edc0
Giovanni Alluvatti <galluvatti@MBP-di-Giovanni.station>
2018-04-11 18:15:21 +0200

    WIP : collision check

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 // Enemies our player must avoid
 var Enemy = function(x, y) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
+  //Coordinates in the canvas used to draw the enemy
   this.x = x;
   this.y = y;
 };
@@ -16,7 +14,16 @@ Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
+  this.x = this.x + 101 * dt;
 };
+
+Enemy.prototype.getX = function() {
+  return this.x;
+}
+
+Enemy.prototype.getY = function() {
+  return this.y;
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -26,6 +33,7 @@ Enemy.prototype.render = function() {
 //This is the Player class, representing the player
 var Player = function() {
   this.sprite = 'images/char-boy.png';
+  //Coordinates in the canvas used to draw the player
   this.x = 202;
   this.y = 373.5;
 };
@@ -36,7 +44,10 @@ Player.prototype.reset = function() {
 };
 
 Player.prototype.update = function() {
-  this.render();
+  for(enemy of allEnemies) {
+    if(this.x === enemy.getX() && this.y === enemy.getY())
+      ctx.init();
+  }
 };
 
 Player.prototype.render = function() {
@@ -44,26 +55,26 @@ Player.prototype.render = function() {
 };
 
 //This function is used to update player coordinates in canvas according to the
-//pressed key
+//pressed key. It also performs some check, to avoid player going off screen
 Player.prototype.handleInput = function(direction) {
   switch (direction) {
     case 'up':
-      this.y = this.y - 83;
-      if(this.y < 0)
-        this.reset();
-      break;
+    this.y = this.y - 83;
+    if(this.y < 0)
+    this.reset();
+    break;
     case 'down':
-      if(this.y + 83 <= 373.5)
-        this.y = this.y + 83;
-      break;
+    if(this.y + 83 <= 373.5)
+    this.y = this.y + 83;
+    break;
     case 'left':
-      if(this.x - 101 >= 0)
-        this.x = this.x - 101;
-      break;
+    if(this.x - 101 >= 0)
+    this.x = this.x - 101;
+    break;
     case 'right':
-      if(this.x + 101 < 505)
-      this.x = this.x + 101;
-      break;
+    if(this.x + 101 < 505)
+    this.x = this.x + 101;
+    break;
     default:
 
   }
@@ -73,7 +84,17 @@ Player.prototype.handleInput = function(direction) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const player = new Player();
-const allEnemies = [new Enemy(0, 83)];
+const allEnemies = [
+  new Enemy(0, 50.5),
+  new Enemy(-300, 50.5 + 83),
+  new Enemy(-600, 50.5 + 83),
+  new Enemy(-200, 50.5 + 83*2),
+  new Enemy(-400, 50.5 + 83*3),
+  new Enemy(-600, 50.5 + 83),
+  new Enemy(-900, 50.5 + 83),
+  new Enemy(-1000, 50.5 + 83*2),
+  new Enemy(-800, 50.5 + 83*3)
+];
 
 
 // This listens for key presses and sends the keys to your
